e55399de96bfef408c82995eb542b2ea168785b8
BlaZ <connerwithahat@gmail.com>
2024-04-08 13:21:23 -0600

    Update SaveSystem.cs

--- DeckOfSpells/Assets/Scripts/SaveSystem.cs
+++ DeckOfSpells/Assets/Scripts/SaveSystem.cs
@@ -1,5 +1,5 @@
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+//using System.IO;
+//using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public static class SaveSystem
@@ -7,31 +7,31 @@ public static class SaveSystem
     
     public static void SaveData (GameObject objectToSave)
     {
-        BinaryFormatter formatter = new BinaryFormatter();
+        //BinaryFormatter formatter = new BinaryFormatter();
         //string path = System.IO.Path.Combine(Application.persistentDataPath, "/player.idk");
         string path = Application.persistentDataPath + "/player.idk";
-        FileStream stream = new FileStream(path, FileMode.Create);
+        //FileStream stream = new FileStream(path, FileMode.Create);
 
         SaveData data = new SaveData(objectToSave);
 
-        formatter.Serialize(stream, data);
-        stream.Close();
+        //formatter.Serialize(stream, data);
+        //stream.Close();
     }
 
     public static SaveData LoadData()
     {
 		//string path = System.IO.Path.Combine(Application.persistentDataPath, "/player.idk");
-		string path = Application.persistentDataPath + "/player.idk";
-		if (File.Exists(path))
-        {
-            BinaryFormatter formatter = new BinaryFormatter();
-            FileStream stream = new FileStream (path, FileMode.Open);
+		//string path = Application.persistentDataPath + "/player.idk";
+		//if (File.Exists(path))
+        //{
+            //BinaryFormatter formatter = new BinaryFormatter();
+            //FileStream stream = new FileStream (path, FileMode.Open);
 
-            SaveData data = formatter.Deserialize(stream) as SaveData;
-            stream.Close();
+            //SaveData data = formatter.Deserialize(stream) as SaveData;
+            //stream.Close();
 
-            return data;
-        }
+            //return data;
+        //}
 
         return null;
     }
